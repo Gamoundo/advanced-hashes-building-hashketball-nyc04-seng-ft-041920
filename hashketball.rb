2b37e7c3ec5cfc3d35d25c3759cda70dcc7b8ff7
Akil Griffiths <akilgg@gmail.com>
2020-04-29 16:31:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,15 @@ def game_hash
   game = {:home => 
  {team_name: "Brooklyn Nets",
    colors: ['Black', 'White'],
-   players: [{}]
+   players: [{:player_name => "Alan Anderson",
+:number =>,
+:shoe =>,
+:points =>,
+:rebounds =>,
+:assists =>,
+:steals =>,
+:blocks =>,
+:slam_dunks =>,}]
    }, 
  :away => {
    team_name: {},
