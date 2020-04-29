99f3b2c1ff59486c3002b02d692fffc766dcfe28
Akil Griffiths <akilgg@gmail.com>
2020-04-29 16:36:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,7 +12,16 @@ def game_hash
 :assists => 12,
 :steals => 3,
 :blocks => 1,
-:slam_dunks => 1,}]
+:slam_dunks => 1}, 
+{:player_name => "",
+:number => ,
+:shoe => ,
+:points => ,
+:rebounds => ,
+:assists => ,
+:steals => ,
+:blocks => ,
+:slam_dunks => }]
    }, 
  :away => {
    team_name: {},
